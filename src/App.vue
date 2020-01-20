95ef79e25b65004654aaf029a34ca28e89258763
dc2348 <dc2348@hanmail.net>
2020-01-20 16:37:58 +0900

    FEAT:Add body and change ko font

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,10 @@
     <!-- Bootstrap-Vue start-->
     <b-navbar type="light" variant="faded">
       <b-navbar-brand href="https://dc2348.github.io"> 
-        <!-- <img src="./assets/images/logo_profile_with_line_30x30.png" class="d-inline-block align-top" alt="profile logo"> -->
-        Frontend 개발
+        <img src="./assets/images/icon_macaroon_30x30.jpg" class="d-inline-block align-top" alt="profile logo">
+        <span lang="en"> Frontend</span> 개발
       </b-navbar-brand>
-      <b-navbar-nav class="ml-auto">
+      <b-navbar-nav class="ml-auto" lang="en">
         <b-nav-item href="https://dc2348.github.io">Home</b-nav-item>
         <b-nav-item href="#" disabled>Blog</b-nav-item>
         <b-nav-item href="#" disabled>Portfolio</b-nav-item>
@@ -32,7 +32,7 @@ import Vue from 'vue'
 import HomeBody from './components/HomeBody.vue'
 
 // Bootstrap-Vue start
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import { BootstrapVue, IconsPlugin} from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
@@ -57,11 +57,14 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
   margin-left: auto;
   margin-right: auto;
+  font-family: 'Nanum Gothic Coding', monospace;
+}
+
+*:lang(en) { 
   font-family: 'Ubuntu', sans-serif;
 }
 
@@ -77,10 +80,11 @@ export default {
 }
 
 .footer {
-   position: fixed;
-   bottom: 0;
-   width: 100%;
-   text-align: left;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  text-align: left;
+  padding: 16px;
 }
 
 .footer .copyright{
--- src/components/HomeBody.vue
+++ src/components/HomeBody.vue
@@ -1,21 +1,49 @@
 <template>
   <div class="body">
-    <h1>{{ msg }}</h1>
+      <div>
+      <b-card img-src="../assets/images/background_macaroon.jpg" img-alt="Card image" img-top>
+        <b-card-text>
+          쇼핑몰 개발을 하며 경험하는 것들을 기록해보려고 합니다.
+        </b-card-text>
+        <b-button lang="en" href="#" variant="outline-secondary">Go Blog</b-button> <span> </span>
+        <b-button lang="en" href="#" variant="outline-secondary">Go Portfolio</b-button>
+      </b-card>
+    </div>
   </div>
+  
 </template>
 
 <script>
+import Vue from 'vue'
+
 export default {
-  name: 'HelloWorld',
+  name: 'Homebody',
   props: {
     msg: String
   }
 }
+
+// Bootstrap-Vue start
+import { BootstrapVue, CardPlugin, ButtonPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+Vue.use(CardPlugin)
+Vue.use(ButtonPlugin)
+// Bootstrap-Vue end
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .body {
   margin-top: 60px;
+  padding: 16px;
+  font-family: 'Nanum Gothic Coding', monospace;
+}
+
+*:lang(en) { 
+  font-family: 'Ubuntu', sans-serif;
 }
 </style>
