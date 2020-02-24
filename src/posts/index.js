d2d8df18e5812e758b17498a6cf4ff3d61ba4d28
dc2348 <dc2348@hanmail.net>
2020-02-25 08:21:07 +0900

    CLN:Refactor blog

---
+++ src/posts/index.js
@@ -0,0 +1,27 @@
+import Post1 from '@/posts/aws/2020-01-22-use-aws-cli.md'
+import Post2 from '@/posts/aws/2020-01-23-clone-codecommit-repo-using-aws-cli.md'
+import Post3 from '@/posts/vue/2020-02-20-vue-markdown-loader.md'
+import Post4 from '@/posts/vue/2020-02-19-async-programming.md'
+
+export default [
+    {
+        mdfile: Post1,
+        title:'AWS CLI 사용하기',
+        date:'2020-01-22 10:00:00 +0900'
+    },
+    {
+        mdfile: Post2,
+        title:'AWS CLI를 이용하여 CodeCommit Clone하기',
+        date:'2020-01-23 16:00:00 +0900'
+    },
+    {
+        mdfile: Post3,
+        title:'Vue-CLI3에서 markdown 파일 로드하기',
+        date:'2020-02-20 09:00:00 +0900'
+    },
+    {
+        mdfile: Post4,
+        title:'비동기적 프로그래밍',
+        date:'2020-02-19 15:00:00 +0900'
+    }
+];
--- src/views/BlogView.vue
+++ src/views/BlogView.vue
@@ -1,50 +1,23 @@
 <template>
   <div>
-    <router-link :to="{ name: 'posts', params: { id: 1, category:'aws' }}">
+    <router-link v-for="item in posts" :key="item.title" :to="{ name: 'posts', params: { id: 1, category:'aws', component: item.mdfile}}">
       <b-card  class="mb-3">
-        <b-card-title>AWS CLI 사용하기</b-card-title>
-        <b-card-text class="small text-muted">2020-01-22 10:00:00 +0900</b-card-text>
+        <b-card-title>{{ item.title }}</b-card-title>
+        <b-card-text class="small text-muted">{{ item.date }}</b-card-text>
       </b-card>
     </router-link>
-    <router-link :to="{ name: 'posts', params: { id: 2, category:'aws' }}">
-      <b-card  class="mb-3">
-        <b-card-title>AWS CLI를 이용하여 CodeCommit Clone하기</b-card-title>
-        <b-card-text class="small text-muted">2020-01-23 16:00:00 +0900</b-card-text>
-      </b-card>
-    </router-link>
-    <router-link :to="{ name: 'posts', params: { id: 3, category:'vue' }}">
-      <b-card  class="mb-3">
-        <b-card-title>Vue-CLI3에서 markdown 파일 로드하기</b-card-title>
-        <b-card-text class="small text-muted">2020-02-20 09:00:00 +0900</b-card-text>
-      </b-card>
-    </router-link>
-    <router-link :to="{ name: 'posts', params: { id: 4, category:'vue' }}">
-      <b-card  class="mb-3">
-        <b-card-title>비동기적 프로그래밍</b-card-title>
-        <b-card-text class="small text-muted">2020-02-19 15:00:00 +0900</b-card-text>
-      </b-card>
-    </router-link>
-    <!-- <router-link :to="{ name: 'blogDetail', params: { id: 1, category:'aws' }}">
-      <b-card class="mb-3">
-        <b-card-title>{{ POST1.title }}</b-card-title>
-        <b-card-text>
-          {{ POST1.summary }}
-        </b-card-text>
-        <b-card-text class="small text-muted">{{ POST1.date }}</b-card-text>
-      </b-card>
-    </router-link> -->
   </div>
 </template>
 
 <script>
-// import POST1 from '@/posts/aws/2020-01-22-use-aws-cli.js'
+import posts from '@/posts/index.js'
 
 export default {
-  // data () {
-  //   return {
-  //     POST1
-  //   }
-  // }
+  data () {
+    return {
+      posts
+    }
+  }
 };
 </script>
 
