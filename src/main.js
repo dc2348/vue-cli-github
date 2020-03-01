4ba8ecc53499b8bfa2362388b4a9079b887e2caf
unknown <dc2348@nsmall.com>
2020-03-01 22:41:33 +0900

    FEAT:Add git-alt fontawesome

--- src/main.js
+++ src/main.js
@@ -3,9 +3,9 @@ import App from './App.vue'
 import { router } from "./routes/index.js"
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faVuejs, faAws, faJs, faNpm } from '@fortawesome/free-brands-svg-icons'
+import { faVuejs, faAws, faJs, faNpm, faGitAlt } from '@fortawesome/free-brands-svg-icons'
  
-library.add( faVuejs, faAws, faJs, faNpm )
+library.add( faVuejs, faAws, faJs, faNpm, faGitAlt )
  
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
